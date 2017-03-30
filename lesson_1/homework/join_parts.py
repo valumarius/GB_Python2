b0208ce97c63fbab1780ae6324c456b450fb8df1
Petr Antonov <antpeter@gmail.com>
2017-03-30 21:22:54 +0300

    Реализация алгоритма склеивания файла

---
+++ lesson_1/homework/join_parts.py
@@ -0,0 +1,28 @@
+import zipfile
+import hashlib
+import os
+
+with zipfile.ZipFile("files/file1.zip") as zip_ref:
+    zip_ref.extractall()
+
+with open("file1/parts.md5") as parts_md5:
+    hashes = parts_md5.read().split('\n')
+    hashes.remove('')
+
+dir_list = os.listdir("file1")
+dir_list.remove('parts.md5')
+
+final_list = []
+counter = 0
+for hash in hashes:
+    for file in dir_list:
+        with open("file1/{}".format(file), "rb") as f:
+            content = f.read()
+            h = hashlib.md5()
+            h.update(content)
+            counter += 1
+            print(counter)
+            if h.hexdigest() == hash:
+                final_list.append(content)
+                with open("blablablah", "ba") as f_out:
+                    r = f_out.write(content)
