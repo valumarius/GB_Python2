ab181eaa19e87c34802248d77448417806ad7f67
Petr Antonov <antpeter@gmail.com>
2017-04-02 20:26:52 +0300

    Реализовал клиент, формирующий пакет длиной 8байт (заголовок, дата, время, тип транзакции). Реализовал сервер, "расшифровывающий" пакет от клиента.

--- lesson_2/homework/client.py
+++ lesson_2/homework/client.py
@@ -1,20 +1,46 @@
-
-# Клиент игры "Запоминалка"
-
 import socket
+from datetime import datetime, time
 
 HOST, PORT = 'localhost', 9999
+
+
+def date_encode(now):
+    """
+    Функция кодирования даты (год, месяц, число) в 2 байта
+    :return: 
+    """
+    date = 512 * (now.year - 2000) + 32 * now.month + now.day
+    return date.to_bytes(2, 'big')
+
+
+def seconds_from_midnight(now):
+    """
+    Функция кодирования секунд начиная от полуночи в 3 байта
+    :return: 
+    """
+    midnight = datetime.combine(now.date(), time(0))
+    delta = now - midnight
+    return delta.seconds.to_bytes(3, 'big')
 
 print('Клиент запущен')
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-sock.sendall(bytes('I_WANNA_PLAY', 'utf-8'))
+# now = datetime.now()
+print('Введите тип транзакции: \n'
+      '0 - сервисная \n'
+      '1 - платежная \n'
+      '2 - инкассация')
+transact_type = int(input())
+ttype_bin = transact_type.to_bytes(1, 'big')
 
-recvd = str(sock.recv(1024), 'utf-8')
+packet = b'zz' + date_encode(datetime.now()) + \
+         seconds_from_midnight(datetime.now()) + ttype_bin
+sock.sendall(packet)
+
+recvd = str(sock.recv(1024), 'ascii')
 
 print(recvd)
 
 sock.close()
 
-# __add__  ->  +
--- lesson_2/homework/server.py
+++ lesson_2/homework/server.py
@@ -2,21 +2,49 @@
 # Сервер игры "Запоминалка"
 
 import socketserver
+from datetime import datetime
 import random
+
+
+
+
 
 class MemTCPHandler(socketserver.BaseRequestHandler):
 
+    def intdate_to_datetime(self, intdate, intseconds):
+        self.year = intdate // 512 + 2000
+        self.month = (intdate % 512) // 32
+        self.day = (intdate % 512) % 32
+        self.hours = intseconds // 3600
+        self.minutes = (intseconds % 3600) // 60
+        self.seconds = (intseconds % 3600) % 60
+        return datetime(self.year, self.month, self.day,
+                        self.hours, self.minutes, self.seconds)
+
     def handle(self):
-        self.data = self.request.recv(1024).decode()
-        print("Клиент {} сообщает {}".format(self.client_address[0], self.data))
+        self.data = self.request.recv(1024)
+        self.header = str(self.data[:2], 'ascii')
+        self.intdate = int.from_bytes(self.data[2:4], 'big')
+        self.intseconds = int.from_bytes(self.data[4:7], 'big')
+        self.datetime = self.intdate_to_datetime(self.intdate,
+                                                 self.intseconds)
+        self.transact_type = self.data[7]
+        print("Клиент {} сообщает {} {} {}".format(self.client_address[0],
+                                                   self.header,
+                                                   self.datetime,
+                                                   self.transact_type))
 
-        if self.data == "I_WANNA_PLAY":
-            nums = random.sample(range(1,100), 18) * 2
-            random.shuffle(nums)
-            s_nums = [str(n) for n in nums]
-            s_nums = ';'.join(s_nums)
-
-            self.request.sendall(bytes('NUMS;'+s_nums, 'utf-8'))
+        if self.header == 'zz':
+            print("Transaction")
+            if self.transact_type == 0:
+                print("Сервисная транзакция")
+            elif self.transact_type == 1:
+                print("Платежная транзакция")
+            elif self.transact_type == 2:
+                print("Инкассация")
+            else:
+                print('Неизвестный запрос')
+            self.request.sendall(b'ok')
         else:
             print('Неизвестный запрос')    
 
@@ -24,7 +52,7 @@ class MemTCPHandler(socketserver.BaseRequestHandler):
 HOST, PORT = 'localhost', 9999
 
 server = socketserver.TCPServer((HOST, PORT), MemTCPHandler)  
-print('Сервер игры запущен')
+print('Сервер препроцессинга запущен')
 
 server.serve_forever()
 
